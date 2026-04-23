17ecf46b911789ba4faf0d5b1da4649a27b3c325
Harshitha <ncharshi2005@gmail.com>
2026-04-23 06:56:49 +0530

    'g'

--- src/main/java/com/devops/lab/Calculator.java
+++ src/main/java/com/devops/lab/Calculator.java
@@ -1,10 +1,4 @@
 public class Calculator {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
-    }
+    public int add(int a, int b) { return a + b; }
+    public int multiply(int a, int b) { return a * b; }
 }
